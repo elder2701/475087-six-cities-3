96c736677c1c49d49c7923e35aa617464ca93ca0
Alexy Rumyancev <elder111@yandex.ru>
2020-02-19 22:07:25 +0300

    createNodeMock
    Добавлена мок функция для ref

--- src/components/map/map.test.js
+++ src/components/map/map.test.js
@@ -2,7 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Map from "./map.jsx";
 
+function createNodeMock(element) {
+  if (element.type === `section`) {
+    return {};
+  }
+  return null;
+}
+
 it(`<Map /> sould be render`, () => {
+  const options = {createNodeMock};
   const tree = renderer
     .create(
         <Map
@@ -10,7 +18,7 @@ it(`<Map /> sould be render`, () => {
             [51.38333, 6.9],
             [54.38333, 5.9]
           ]}
-        />
+        />, options
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
