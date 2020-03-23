6dd73e19aac99338f670553a2091166d853a55ae
Alexy Rumyancev <elder111@yandex.ru>
2020-03-23 23:21:24 +0300

    Ошибка в тесте
    Исравлена ошибка в сквозном тесте PlaceDetails

--- src/components/place-details/place-details.e2e.test.js
+++ src/components/place-details/place-details.e2e.test.js
@@ -1,17 +1,7 @@
 import React from "react";
-import Enzyme, {shallow, mount} from "enzyme";
+import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import PlaceDetails from "./place-details.jsx";
-import createMapBlock from "../map/create-map-block.js";
-import cofigureStore from "redux-mock-store";
-import {Provider} from "react-redux";
-import NameSpace from "../../reducer/name-space.js";
-import thunk from "redux-thunk";
-import {createAPI} from "../../api.js";
-
-const api = createAPI(()=>{});
-
-const mockStore = cofigureStore([thunk.withExtraArgument(api)]);
+import {PlaceDetails} from "./place-details.jsx";
 
 const offer = {
   id: 1,
@@ -80,85 +70,17 @@ const mockEvent = {
 
 describe(`Mouse events`, () => {
   const handleChangeFavorite = jest.fn();
-  const store = mockStore({
-    [NameSpace.DATA]: {
-      offers: {
-        Amsterdam: {
-          city: {
-            name: `Amsterdam`,
-            location: {
-              latitude: 50.846557,
-              longitude: 4.351697,
-              zoom: 13
-            }
-          },
-          offers: [
-            {
-              images: [],
-              title: `The house among olive `,
-              rating: 3.4,
-              type: `apartment`,
-              bedrooms: 3,
-              price: 148,
-              goods: [
-                `Washer`,
-                `Breakfast`,
-                `Laptop friendly workspace`,
-                `Dishwasher`,
-                `Towels`,
-                `Baby seat`,
-                `Fridge`,
-                `Air conditioning`
-              ],
-              description: `I am happy to welcome you to my apartment in the city center! Three words: location, cosy and chic!`,
-              location: {
-                latitude: 50.869557,
-                longitude: 4.332697,
-                zoom: 16
-              },
-              id: 1,
-              previewImage: ``,
-              isFavorite: false,
-              isPremium: true,
-              maxAdults: 4,
-              hostId: 25,
-              hostName: `Angelina`,
-              hostIsPro: true,
-              hostAvatarUrl: `img/avatar-angelina.jpg`
-            }
-          ]
-        }
-      }
-    },
-    [NameSpace.CITY]: {
-      city: `Amsterdam`
-    },
-    [NameSpace.USER]: {
-      authorizationStatus: `AUTH`,
-      userInfo: {
-        "id": 1,
-        "email": `O@gmail.com`,
-        "name": `O`,
-        "avatar_url": `/static/avatar/2.jpg`,
-        "is_pro": false
-      }
-    },
-    [NameSpace.OFFER]: {
-      offer: -1,
-      offerAround: [],
-      comments: [],
-      isSendComment: true
-    }
-  });
-  createMapBlock();
-  const screen = mount(
-      <Provider store={store}>
-        <PlaceDetails {...offer} updateStatus={handleChangeFavorite}/>
-      </Provider>
+  const screen = shallow(
+      <PlaceDetails
+        {...offer}
+        updateStatus={handleChangeFavorite}
+        cityInfo={{location: 0.0}}
+        nearPlaces={[]}
+      />
   );
 
   it(`Button click`, () => {
-    const button = screen.find(`button`).at(0);
+    const button = screen.find(`button`);
     button.simulate(`click`, mockEvent);
     expect(handleChangeFavorite).toHaveBeenCalledTimes(1);
   });
